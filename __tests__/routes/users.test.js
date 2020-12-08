0baff082f667570e1c8a973487a23e8d51eea1e3
Jesús Rodríguez <mc.ireiser@gmail.com>
2020-12-08 19:17:28 -0400

    Update __tests__/routes/users.test.js
    Co-authored-by: Jose D. Gomez R. <jose.gomez@suse.com>

--- __tests__/routes/users.test.js
+++ __tests__/routes/users.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("Users route", () => {
-  it("Should render with the text `respond with a resource`", async () => {
+  it("renders with `respond with a resource`", async () => {
     const res = await request(app).get("/users");
 
     expect(res.statusCode).toBe(200);
