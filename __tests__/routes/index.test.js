c93c7ee1a783787c59279ebcccc6c91cea1a6226
Jose D. Gomez R <jose.gomez@suse.com>
2020-12-08 23:34:26 +0100

    Adding a sample test for the index endpoint
    Sample test file checking that the index controller works as
    expected.

---
+++ __tests__/routes/index.test.js
@@ -0,0 +1,14 @@
+const request = require('supertest')
+const app = require('../../app')
+
+describe('Index Controller', () => {
+
+  it('Renders successfully', async () => {
+    const res = await request(app)
+      .get('/')
+
+    expect(res.statusCode).toBe(200)
+    expect(res.get('Content-Type')).toMatch('text/html')
+  })
+})
+
