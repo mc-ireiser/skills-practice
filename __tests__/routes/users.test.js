5324131d7a581c044c0e6c00f08f9d0171d392d0
Jose D. Gomez R <jose.gomez@suse.com>
2020-12-09 01:55:11 +0100

    Bootstrapping Sequelize & first model
    Added all the bootstrapping by sequelize-cli, 1st model & configs.
    
    Created the first model & implemented fetching from the users
    controller.
    
    Altered the test accordingly.

--- __tests__/routes/users.test.js
+++ __tests__/routes/users.test.js
@@ -2,10 +2,13 @@ const request = require("supertest");
 const app = require("../../app");
 
 describe("Users route", () => {
-  it("renders with `respond with a resource`", async () => {
-    const res = await request(app).get("/users");
+  describe('When there is no database records', () => {
+    it("renders an empty list", async () => {
+      const res = await request(app).get("/users")
 
-    expect(res.statusCode).toBe(200);
-    expect(res.text).toBe("respond with a resource");
+      expect(res.statusCode).toBe(200)
+      expect(res.type).toBe('application/json')
+      expect(res.body).toStrictEqual([])
+    });
   });
 });
